54ec838fffe8978d453a37adbbd2258e8962cbbc
raven1911 <vuduclam1911@gmail.com>
2024-10-03 01:00:56 +0700

    init ex10 done

--- Lab2_vxl_f1/Core/Src/Lab2.c
+++ Lab2_vxl_f1/Core/Src/Lab2.c
@@ -29,7 +29,7 @@ unsigned characterHEX[][8] = {	// sum = 38 characterHEX
 {0xFF, 0x00, 0x00, 0xFC, 0xFC, 0xFC, 0xFC, 0xFF},//L 0
 {0xFF, 0xC0, 0x80, 0x33, 0x33, 0x80, 0xC0, 0xFF},//A 1
 {0x00, 0x00, 0xAF, 0xCF, 0xCF, 0xAF, 0x00, 0x00},//M 2
-{0xCF, 0x87, 0x83, 0xC1, 0xC1, 0x83, 0x87, 0xCF}// hình trái tim, kí hiệu là '&' 4
+{0xCF, 0x87, 0x83, 0xC1, 0xC1, 0x83, 0x87, 0xCF}// hình trái tim, kí hiệu là '&' 3
 };
 
 
@@ -59,25 +59,26 @@ void updateLEDMatrix (int index) {
 
 uint8_t data_character = 0;
 uint8_t counter = MAX_LED_MATRIX - 1;
-uint8_t counter_shift = 0;
 void animation_shift_left(){
 	for(int i = 0; i < MAX_LED_MATRIX; i++){
 		if(i <  7){
 			matrix_buffer_col[i] = matrix_buffer_col[i+1];
 		}
 		if(i >= 7){
-			matrix_buffer_col[i] = characterHEX[data_character][MAX_LED_MATRIX - 1 - counter];
+			matrix_buffer_col[i] = characterHEX[data_character][(MAX_LED_MATRIX - 1) - counter];
+			counter--;
+		}
+		if(counter <= 0) {
+			data_character++;
+			counter = MAX_LED_MATRIX - 1;
+			if(data_character > 3) data_character = 0;
+
 		}
 	}
-	if(counter == 0) counter = MAX_LED_MATRIX - 1;
-	if(counter_shift >= MAX_LED_MATRIX - 1) {
-		if(data_character > 3) data_character = 0;
-		data_character++;
-		counter_shift = 0;
 
-	}
-	counter--;
-	counter_shift++;
+
+
+
 
 }
 
